096f9658391a25f27a3228740a68b2a2b60df40a
Maurice <mauricium.maurice@yahoo.com>
2019-09-13 10:09:36 +0100

    New commit

--- cypress/integration/createride.js
+++ cypress/integration/createride.js
@@ -1,15 +1,15 @@
 describe('User create ride', () =>{
     it('Should create a new ride', () =>{
-        cy.visit('/create-ride')
+        cy.visit('/auth/login')
         cy.get('input[name="email"]').type('morsxossxs@gmail.com')
         cy.get('input[name="password"]').type('qwertyuiop123')
         cy.get('button.btn').click()
         cy.get('a[href="/create-ride"]').click()
         cy.get('input[name="location"]').type('lekki')
         cy.get('input[name="destination"]').type('ikeja')
-        cy.get('input[name="departuretime"]').type('10:00:30')
-        cy.get('input[name="departuretime"]').type('12-12-1999')
-        cy.get('input[name="departuretime"]').type('2')
+        cy.get('input[name="departuretime"]').type('10:00:00')
+        cy.get('input[name="datecreated"]').type('1999-12-12')
+        cy.get('input[name="seatsavailable"]').type('2')
         cy.get('button.btn').click();
     })
 })
